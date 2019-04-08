82cf60428b12b641b8e75b8bca549997d556b273
EC2 Default User <ec2-user@ip-172-31-95-209.ec2.internal>
2019-04-08 06:55:01 +0000

    12.1 es.finito

--- vita-ambiente/src/app/app.module.ts
+++ vita-ambiente/src/app/app.module.ts
@@ -1,14 +1,25 @@
+
+import {   //Importare le funzionalità nella nostra applicazione
+ FormsModule,
+ ReactiveFormsModule
+} from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { FormDemoComponent } from './form-demo/form-demo.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormDemoComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,  //Anche qui
+   ReactiveFormsModule,
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
